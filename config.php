9b21ab114b67adc460e419ee34d64baf5054682c
kulomambu <77854129+kulomambu@users.noreply.github.com>
2021-01-27 04:02:26 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// cookie
+$__cfduid   = "__cfduid=d3d94ed454b59ea2c0aad8b04739558d11611693957";
+$PHPSESSID  = "PHPSESSID=bam0dpdue84tfgj5ra5hku4oti";
+// address ltc
+$wallet_ltc = "MAbfLpz22DCv5CoJq1KH8THfuaQqTTxJSV";
+
+
+?>
